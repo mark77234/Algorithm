cd27785736a4a1080adc00b9f6c84010a9a387f6
BYEONGCHAN LEE <mark77234@naver.com>
2026-03-28 00:09:37 +0900

    [Silver IV] Title: Maximum Subarray, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10211. Maximum Subarray/Maximum Subarray.py
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.py
@@ -4,12 +4,10 @@ for i in range(r):
     n = int(input())
     arr = list(map(int,input().split()))
     
-    tot = arr[0]
-    answer = arr[0]
-    for a in arr[1:]:
-        tot = max(a, tot + a)
-        answer = max(tot,answer)
-        if tot < 0:
-            tot = 0
+    dp = [0] * n
+    dp[0] = arr[0]
+    for i in range(1,n):
+        dp[i] = max(arr[i],dp[i-1]+ arr[i])
+    print(max(dp))
     
-    print(answer)
+    
