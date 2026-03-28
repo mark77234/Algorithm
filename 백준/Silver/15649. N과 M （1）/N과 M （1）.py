8547bdd37b9a569b49f8b26aad9d8fff92826b19
BYEONGCHAN LEE <mark77234@naver.com>
2026-03-28 13:03:29 +0900

    [Silver III] Title: N과 M (1), Time: 140 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,13 +1,18 @@
-from itertools import permutations
 
-number, size = map(int,input().split())
-
-numbers= []
-for i in range(1,number+1):
-    numbers.append(i)
-
-answer = list(permutations(numbers,size))
-
-for a in answer:
-    print(' '.join(map(str,a)))
+def backtracking():
+    if len(array) == m:
+        print(" ".join(map(str,array)))
+        return
     
+    for i in range(1, n+1):
+        if i not in array:
+            array.append(i)
+            backtracking()
+            array.pop()
+
+
+n, m = map(int,input().split())
+
+array= []
+
+backtracking()
