37a80c812ed44fe04f47d779ecedb6b5363b135f
BYEONGCHAN LEE <mark77234@naver.com>
2026-06-06 11:38:31 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,7 +1,5 @@
 def solution(n):
-    cnt = 1
-    while n > 7:
-        n -= 7
-        cnt +=1
-    
-    return cnt
+    if n % 7 == 0:
+        return n // 7
+    else:
+        return n // 7 + 1
